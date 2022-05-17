02b2747a58803b58934608007a4ffedc4569a5f6
WordChained <talbd51@gmail.com>
2022-05-17 11:07:56 +0300

    finished filter in first table

--- src/cmps/PopulationVaccination/PopulationVaccination.jsx
+++ src/cmps/PopulationVaccination/PopulationVaccination.jsx
@@ -1,15 +1,17 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { BigCard } from '../../UI/BigCard';
-import styles from './PopulationVaccination.module.css';
-import { Sorter } from '../../UI/Sorter';
-import { CoronaTable } from '../../UI/CoronaTable';
-import { IoIosCloseCircle } from 'react-icons/io';
-import { MOCK_DATA } from '../../mock-data';
-import { PercentBar } from '../../UI/PercentBar';
+import React, { useState, useRef, useEffect } from "react";
+import { BigCard } from "../../UI/BigCard";
+import styles from "./PopulationVaccination.module.css";
+import { Sorter } from "../../UI/Sorter";
+import { CoronaTable } from "../../UI/CoronaTable";
+import { IoIosCloseCircle } from "react-icons/io";
+import { MOCK_DATA } from "../../mock-data";
+import { PercentBar } from "../../UI/PercentBar";
 export const PopulationVaccination = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [isText, setIsText] = useState(false);
   const [data, setData] = useState([]);
+  // const [filteredData, setFilteredData] = useState([]);
+  const [suggestions, setSuggestions] = useState([]);
   const inputRef = useRef();
   const onSortClick = () => {
     setIsOpen(!isOpen);
@@ -19,43 +21,46 @@ export const PopulationVaccination = () => {
   };
 
   const onInput = (ev) => {
-    if (ev.target.value.length > 0) setIsText(true);
-    else setIsText(false);
+    if (ev.target.value.length > 0) {
+      setIsText(true);
+    } else setIsText(false);
+    filterSuggestions(ev.target.value);
   };
 
   const onXClick = () => {
-    inputRef.current.value = '';
+    inputRef.current.value = "";
     setIsText(false);
+    filterSuggestions([]);
   };
   const getColorForDailyScore = (score) => {
     switch (true) {
       case score <= 4.5:
-        return '#b8de92';
+        return "#b8de92";
       //green
       case score <= 6:
-        return '#fcfc70';
+        return "#fcfc70";
       //yellow
       case score <= 7.5:
-        return '#f2c580';
+        return "#f2c580";
       //orange
-      case score === 'אין מידע':
-        return 'transparent';
+      case score === "אין מידע":
+        return "transparent";
       default:
-        return '#fa9e8f';
+        return "#fa9e8f";
       //red
     }
   };
   const getLocations = async () => {
     try {
       const locationsData = await MOCK_DATA.getLocationsData(100);
-      console.log('locations:', locationsData);
+      // console.log('locations:', locationsData);
       setData(locationsData);
     } catch (er) {
       console.log(er);
     }
   };
   const sortLocations = (titleIdx, direction) => {
-    console.log('sorting...', titleIdx);
+    // console.log("sorting...", titleIdx);
     switch (+titleIdx) {
       //ישוב
       case 0:
@@ -188,16 +193,39 @@ export const PopulationVaccination = () => {
         break;
     }
   };
+  const filterSuggestions = (term) => {
+    if (!term.length) {
+      setSuggestions([]);
+      return;
+    }
+    // setSuggestions();
+    //returning only 5 options
+    let filteredLocations = data.filter((d) => {
+      return d.location.includes(term);
+    });
+    filteredLocations = filteredLocations.filter((d, idx) => idx < 5);
+    setSuggestions(filteredLocations.map((loc) => loc.location) || []);
+  };
+
+  const filterData = () => {
+    if (!inputRef.current.value.length) getLocations();
+    setData(
+      [...data].filter((d) => d.location.includes(inputRef.current.value))
+    );
+  };
+  const pickSuggestions = (ev) => {
+    inputRef.current.value = ev.target.innerText;
+    setSuggestions([]);
+  };
   useEffect(() => {
     if (!data.length) getLocations();
-    console.log('data changed');
   }, [data]);
 
   return (
     <section className={styles.container}>
-      <BigCard title={'התחסנות לפי ישובים'} isFullWidth={true}>
+      <BigCard title={"התחסנות לפי ישובים"} isFullWidth={true}>
         <Sorter
-          placeholder={'כלל הישובים'}
+          placeholder={"כלל הישובים"}
           isOpen={isOpen}
           openFnction={onSortClick}
         >
@@ -205,31 +233,43 @@ export const PopulationVaccination = () => {
             <div>
               <input
                 ref={inputRef}
-                type='text'
-                placeholder='חפש ישוב'
+                type="text"
+                placeholder="חפש ישוב"
                 onInput={onInput}
               />
               <span
-                className={`${styles.closeIcon} ${isText ? styles.show : ''}`}
+                className={`${styles.closeIcon} ${isText ? styles.show : ""}`}
                 onClick={onXClick}
               >
                 <IoIosCloseCircle />
               </span>
             </div>
             <div className={styles.btnContainer}>
-              <button>אישור</button>
-              <button onClick={onSortCancel}>ביטול</button>
+              {!suggestions.length ? (
+                <>
+                  <button onClick={filterData}>אישור</button>
+                  <button onClick={onSortCancel}>ביטול</button>
+                </>
+              ) : (
+                <div className={styles.suggestions}>
+                  {suggestions.map((loc, idx) => (
+                    <span onClick={pickSuggestions} key={idx}>
+                      {loc}
+                    </span>
+                  ))}
+                </div>
+              )}
             </div>
           </div>
         </Sorter>
         <CoronaTable
           titles={[
-            'ישוב',
-            '% מתחסנים מנה ראשונה ',
-            '% מתחסנים מנה שנייה ',
-            '% מתחסנים מנה שלישית ',
-            'חולים פעילים לכל 10,000 נפש',
-            'ציון יומי מחושב',
+            "ישוב",
+            "% מתחסנים מנה ראשונה ",
+            "% מתחסנים מנה שנייה ",
+            "% מתחסנים מנה שלישית ",
+            "חולים פעילים לכל 10,000 נפש",
+            "ציון יומי מחושב",
           ]}
           sortFunction={sortLocations}
         >
@@ -242,62 +282,62 @@ export const PopulationVaccination = () => {
                   {row.vaxShot1 && row.vaxShot1 < 90 && (
                     <PercentBar
                       width={row.vaxShot1}
-                      backColor={'#bed8e9'}
-                      frontColor={'#1c7d7e'}
+                      backColor={"#bed8e9"}
+                      frontColor={"#1c7d7e"}
                     />
                   )}
                   <span>
                     {!row.vaxShot1
-                      ? 'אין מידע'
+                      ? "אין מידע"
                       : row.vaxShot1 < 90
-                      ? row.vaxShot1 + '%'
-                      : 'מעל 90%'}
+                      ? row.vaxShot1 + "%"
+                      : "מעל 90%"}
                   </span>
                 </div>
                 <div className={styles.percentContainer}>
                   {row.vaxShot2 && row.vaxShot2 < 90 && (
                     <PercentBar
                       width={row.vaxShot2}
-                      backColor={'#d5dbba'}
-                      frontColor={'#b6ca51'}
+                      backColor={"#d5dbba"}
+                      frontColor={"#b6ca51"}
                     />
                   )}
                   <span>
                     {!row.vaxShot2
-                      ? 'אין מידע'
+                      ? "אין מידע"
                       : row.vaxShot2 < 90
-                      ? row.vaxShot2 + '%'
-                      : 'מעל 90%'}
+                      ? row.vaxShot2 + "%"
+                      : "מעל 90%"}
                   </span>
                 </div>
                 <div className={styles.percentContainer}>
                   {row.vaxShot3 && row.vaxShot3 < 90 && (
                     <PercentBar
                       width={row.vaxShot3}
-                      backColor={'#cbdce7'}
-                      frontColor={'#50cbfd'}
+                      backColor={"#cbdce7"}
+                      frontColor={"#50cbfd"}
                     />
                   )}
                   <span>
                     {!row.vaxShot3
-                      ? 'אין מידע'
+                      ? "אין מידע"
                       : row.vaxShot3 < 90
-                      ? row.vaxShot3 + '%'
-                      : 'מעל 90%'}
+                      ? row.vaxShot3 + "%"
+                      : "מעל 90%"}
                   </span>
                 </div>
                 <div>
-                  {row.activeMorbidPer10K ? row.activeMorbidPer10K : 'אין מידע'}
+                  {row.activeMorbidPer10K ? row.activeMorbidPer10K : "אין מידע"}
                 </div>
                 <div>
                   <div
                     style={{
                       backgroundColor: getColorForDailyScore(
-                        row.dailyScore ? row.dailyScore : 'אין מידע'
+                        row.dailyScore ? row.dailyScore : "אין מידע"
                       ),
                     }}
                   >
-                    {row.dailyScore ? row.dailyScore : 'אין מידע'}
+                    {row.dailyScore ? row.dailyScore : "אין מידע"}
                   </div>
                 </div>
               </div>
